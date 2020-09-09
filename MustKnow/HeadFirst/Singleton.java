83d149382956555e8d3f593098433e1993e11577
LX <522537892@qq.com>
2020-09-09 11:00:44 +0800

    new commit LinkedList

---
+++ MustKnow/HeadFirst/Singleton.java
@@ -0,0 +1,39 @@
+package MustKnow.HeadFirst;
+//hungry
+//public class Singleton {
+//    private  Singleton(){}
+//    private  static Singleton instance = new Singleton();
+//
+//    public static Singleton getInstance() {
+//        return instance;
+//    }
+//}
+//lazy
+//public class Singleton{
+//    private Singleton(){}
+//    private static Singleton instance ;
+//    public static Singleton getInstance(){
+//        if(instance==null){
+//            instance = new Singleton();
+//        }
+//        return instance;
+//    }
+//}
+
+//doublecheck
+public class Singleton{
+    private Singleton(){}
+    private  volatile static Singleton instance ;
+    public static Singleton getInstance(){
+        if(instance ==null){
+            synchronized(Singleton.class){
+                if(instance==null){
+                    instance = new Singleton();
+                }
+            }
+        }
+
+        return instance;
+    }
+}
+
