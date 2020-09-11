1b073e84b13db6e139d74c60d5bf1d4e28b7d0a5
laixin <522537892@qq.com>
2020-09-11 23:16:09 +0800

    2020.0911

--- MustKnow/HeadFirst/Singleton.java
+++ MustKnow/HeadFirst/Singleton.java
@@ -1,13 +1,13 @@
 package MustKnow.HeadFirst;
 //hungry
-//public class Singleton {
-//    private  Singleton(){}
-//    private  static Singleton instance = new Singleton();
-//
-//    public static Singleton getInstance() {
-//        return instance;
-//    }
-//}
+public class Singleton {
+    private  Singleton(){}
+    private  static Singleton instance = new Singleton();
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+}
 //lazy
 //public class Singleton{
 //    private Singleton(){}
@@ -21,19 +21,19 @@ package MustKnow.HeadFirst;
 //}
 
 //doublecheck
-public class Singleton{
-    private Singleton(){}
-    private  volatile static Singleton instance ;
-    public static Singleton getInstance(){
-        if(instance ==null){
-            synchronized(Singleton.class){
-                if(instance==null){
-                    instance = new Singleton();
-                }
-            }
-        }
-
-        return instance;
-    }
-}
+//public class Singleton{
+//    private Singleton(){}
+//    private  volatile static Singleton instance ;
+//    public static Singleton getInstance(){
+//        if(instance ==null){
+//            synchronized(Singleton.class){
+//                if(instance==null){
+//                    instance = new Singleton();
+//                }
+//            }
+//        }
+//
+//        return instance;
+//    }
+//}
 
---
+++ XiaoHongShu/ZhengZE/Main.java
@@ -0,0 +1,38 @@
+package XiaoHongShu.ZhengZE;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        StringBuffer stringBuffer = new StringBuffer(in.nextLine());
+        List<Integer> k = new ArrayList<>();
+        //用一个列表来装括号的匹配
+        //遇见‘（’添加首次匹配位置进去
+        //遇见‘）’从最后一次‘（’匹配位置开始删除字符串
+
+        //记录括号内字符个数
+        for(int i = 0; i < stringBuffer.length();){
+            if(stringBuffer.charAt(i)=='(') {
+                k.add(i);
+                i++;
+            }
+
+            else if(stringBuffer.charAt(i)=='<'&&k.size()==0) {
+                stringBuffer.delete(i - 1, i + 1);
+                i = i - 1;
+            }
+            else if(stringBuffer.charAt(i)==')') {
+                stringBuffer.delete(k.get(k.size() - 1),i + 1);
+                i = k.get(k.size() - 1);
+                k.remove(k.size() - 1);
+            }
+            else
+                i++;
+        }
+        System.out.println(stringBuffer);
+    }
+}
